a7adc31346990cbdb2070dd1105b7006515904f4
Karan Mohan <karan.bhola@simsim.in>
2020-09-01 19:31:26 +0530

    fixed style issues

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+
+if (Platform.OS === 'android') { // only android needs polyfill
+    require('intl'); // import intl object
+    require('intl/locale-data/jsonp/en-IN'); // load the required locale details
+}
 
 AppRegistry.registerComponent(appName, () => App);
